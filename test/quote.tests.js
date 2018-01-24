c0d211fee99abfe128982d56b9e413e6b3b51227
Terry Bonds <terry.bonds@ft.com>
2018-01-24 13:42:26 +0000

    final tidy up

--- test/quote.tests.js
+++ test/quote.tests.js
@@ -1,11 +1,11 @@
 var assert = require('chai').assert;
-var webdriver = require('selenium-webdriver');
-var test = require('selenium-webdriver/testing');
-var QuotePage = require('./quote.page');
+    webdriver = require('selenium-webdriver');
+    test = require('selenium-webdriver/testing');
+    QuotePage = require('./quote.page');
+
+
+//params and variables
 var driver;
-
-
-//params
 const mochaTimeOut = 10000;
 
 
@@ -18,16 +18,18 @@ test.before(function() {
 
 //tests
 test.describe('Valid Postcode', function() {
-    test.it('complete form and get quote', function() {
+    test.it('complete form and check quote', function() {
         this.timeout(mochaTimeOut);
 
+        //load homepage
         var quotePage = new QuotePage(driver);
         quotePage.visit();
+
         // enter post code
         driver.findElement(quotePage.postcode).sendKeys('N1 9PD');
 
         //wait for address list and click
-        driver.wait(webdriver.until.elementLocated(quotePage.address_lookup), 10 * 1000)
+        driver.wait(webdriver.until.elementLocated(quotePage.address_lookup), mochaTimeOut)
         driver.findElement(quotePage.address_lookup).click();
 
         //select number of bedrooms
@@ -40,10 +42,7 @@ test.describe('Valid Postcode', function() {
         quotePage.submit();
 
         //wait for loading to complete
-        driver.wait(webdriver.until.elementIsNotVisible(driver.findElement(quotePage.quote_loader), 10 * 1000))
-            .catch(function(err) {
-                //should probably catch specific error
-            });
+        driver.wait(webdriver.until.stalenessOf(driver.findElement(quotePage.quote_loader), mochaTimeOut))
 
         //assert quote is in expected range 
         driver.findElement(quotePage.quote_text).getText().then(function(text) {
@@ -51,6 +50,12 @@ test.describe('Valid Postcode', function() {
             assert.isTrue(1200 < intQuote && intQuote < 1300);
         });
     });
+});
+
+
+//post test clean up
+test.afterEach(function() {
+    driver.manage().deleteAllCookies();
 });
 
 
